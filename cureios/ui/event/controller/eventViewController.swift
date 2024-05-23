72a0c98c1d3101cff816f1368596403131c8369c
masma-dev <masma.work@gmail.com>
2024-05-23 14:14:19 +0800

    Add Event UI

--- cureios/ui/event/controller/eventViewController.swift
+++ cureios/ui/event/controller/eventViewController.swift
@@ -9,13 +9,29 @@ import UIKit
 
 class eventViewController: UIViewController {
 
+    
+    @IBOutlet var bgView: UIView!
+    @IBOutlet var newsList: UIStackView!
+    @IBOutlet var eventList: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
+    @IBAction func switchTab(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            newsList.isHidden = false
+            eventList.isHidden = true
+           
+            bgView.backgroundColor = #colorLiteral(red: 0.8799392581, green: 0.9148589969, blue: 0.9998951554, alpha: 1)
+        } else {
+            newsList.isHidden = true
+            eventList.isHidden = false
+            bgView.backgroundColor = #colorLiteral(red: 0.8320706487, green: 0.9123230577, blue: 1, alpha: 1)
 
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -26,4 +42,33 @@ class eventViewController: UIViewController {
     }
     */
 
+}
+
+
+extension UIColor {
+    public convenience init?(hex: String) {
+        let r, g, b, a: CGFloat
+
+        if hex.hasPrefix("#") {
+            let start = hex.index(hex.startIndex, offsetBy: 1)
+            let hexColor = String(hex[start...])
+
+            if hexColor.count == 8 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    a = CGFloat(hexNumber & 0x000000ff) / 255
+
+                    self.init(red: r, green: g, blue: b, alpha: a)
+                    return
+                }
+            }
+        }
+
+        return nil
+    }
 }
